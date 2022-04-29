113d60d7dbc54f3901275f50d662b7fdf92dc268
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-04-29 10:38:46 -0700

    refactored code

--- src/OpenSign.js
+++ src/OpenSign.js
@@ -4,11 +4,15 @@ export default function IsOpen({ isOpen }) {
   return (
     //this component takes in isOpen prop and renders differently based on whether isOpen props is true or false
     <div className="is-open">
-      <div className={`red circle ${isOpen === 'red' && 'on'}`}></div>
+      {/* <div className={`red circle ${isOpen === 'red' && 'on'}`}></div>
 
       <div className={`yellow circle ${isOpen === 'yellow' && 'on'}`}></div>
 
-      <div className={`green circle ${isOpen === 'green' && 'on'}`}></div>
+      <div className={`green circle ${isOpen === 'green' && 'on'}`}></div> */}
+
+      {isOpen
+        ? 'My name is Joe Exotic and we are open for business'
+        : 'I am never going to financially recover from this'}
     </div>
   );
 }
