5e7088eb17546c9fab59f4f8e047ca71eef25f9f
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-04-28 17:16:19 -0700

    added commented code for how app.js will look like

--- src/App.js
+++ src/App.js
@@ -2,7 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  //track states for stop lights, animal sizes, and animal parade
+
   return (
+    // how onClick will look for each event
+    // <button onClick={() => setIsOpen('true')}
+    // <button onClick={() => setIsClosed('false')}
+
+    // <button onClick={() => setUnicornSize(UnicornSize + 1)}
+    // <button onClick={() => setLionSize(LionSize + 1)}
+
+    // <button onClick={() => setParade([...parade, 'flamingo'])}>Flamingo</button>
+    // <button onClick={() => setParade([...parade, 'Hippo'])}>Hippo</button>
+    // <button onClick={() => setParade([...parade, 'Camel'])}>Camel</button>
+    // <button onClick={() => setParade([...parade, 'Monkey'])}>Monkey</button>
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
