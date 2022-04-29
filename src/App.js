781755c2168874efdfed5fee8b98d565f1965a83
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-04-29 10:26:16 -0700

    refactored some more

--- src/App.js
+++ src/App.js
@@ -15,18 +15,6 @@ function App() {
   const [traffic, setTraffic] = useState(['cheetah', 'tiger']);
 
   return (
-    // how onClick will look for each event
-    // <button onClick={() => setIsOpen('true')}
-    // <button onClick={() => setIsClosed('false')}
-
-    // <button onClick={() => setUnicornSize(UnicornSize + 1)}
-    // <button onClick={() => setLionSize(LionSize + 1)}
-
-    // <button onClick={() => setParade([...parade, 'flamingo'])}>Flamingo</button>
-    // <button onClick={() => setParade([...parade, 'Hippo'])}>Hippo</button>
-    // <button onClick={() => setParade([...parade, 'Camel'])}>Camel</button>
-    // <button onClick={() => setParade([...parade, 'Monkey'])}>Monkey</button>
-
     <div className="App">
       <div className="fight">
         <div className="monster">
