edd4d9461ede71c828a7eb4641ee43df402d810a
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-05-19 10:29:07 -0700

    created admin page that contains table data which is imported from mytable.js. in app.js hardcoded page with link to zoopage and admin page

--- src/App.js
+++ src/App.js
@@ -1,3 +1,32 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import ZooPage from './ZooPage';
+import AdminPage from './AdminPage.js';
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/admin">Admin</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/admin">
+            <AdminPage />
+          </Route>
+          <Route path="/">
+            <ZooPage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
